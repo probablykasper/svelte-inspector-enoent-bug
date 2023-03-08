861db7318ded76f9e440b825211130edec21b397
Kasper <kasperkh.kh@gmail.com>
2023-03-08 22:56:58 +0100

    Enable inspector

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,12 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	vitePlugin: {
+		experimental: {
+			inspector: true,
+		},
+	},
+
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
